e4a15904ab29ebb26a94f66b3d5b14e22ab36f8a
michel_mhl <michel_mhl@hotmail.com>
2023-05-31 10:04:47 -0300

    Usando a validação personalizada para validar se email ja existe na base, e criamos uma nova busca no repositorio por email

--- src/main/java/com/desenvolver/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/desenvolver/cursomc/repositories/ClienteRepository.java
@@ -1,9 +1,12 @@
 package com.desenvolver.cursomc.repositories;
 
 import com.desenvolver.cursomc.domain.Cliente;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente,Integer> {
+    @Transactional(readOnly = true)
+    Cliente findByEmail(String email);
 }
--- src/main/java/com/desenvolver/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/desenvolver/cursomc/services/validation/ClienteInsertValidator.java
@@ -1,7 +1,9 @@
 package com.desenvolver.cursomc.services.validation;
 
+import com.desenvolver.cursomc.domain.Cliente;
 import com.desenvolver.cursomc.dto.ClienteNewDTO;
 import com.desenvolver.cursomc.enums.TipoCliente;
+import com.desenvolver.cursomc.repositories.ClienteRepository;
 import com.desenvolver.cursomc.resources.resources.exception.FieldMessage;
 
 import java.util.ArrayList;
@@ -10,20 +12,30 @@ import java.util.List;
 import com.desenvolver.cursomc.services.validation.utils.BR;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+    @Autowired
+    private ClienteRepository repo;
+
     @Override
     public void initialize(ClienteInsert ann) {
     }
+
     @Override
     public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
         List<FieldMessage> list = new ArrayList<>();
 
-     if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())){
-         list.add(new FieldMessage("cpfOuCnpj","CPF invalido"));
-     }
-     if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())){
-         list.add(new FieldMessage("cpfOuCnpj","CNPJ invalido"));
-     }
+        if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+            list.add(new FieldMessage("cpfOuCnpj", "CPF invalido"));
+        }
+        if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+            list.add(new FieldMessage("cpfOuCnpj", "CNPJ invalido"));
+        }
+        Cliente aux = repo.findByEmail(objDto.getEmail());
+        if(aux != null){
+            list.add(new FieldMessage("email","Email ja existente"));
+        }
 
         for (FieldMessage e : list) {
             context.disableDefaultConstraintViolation();
